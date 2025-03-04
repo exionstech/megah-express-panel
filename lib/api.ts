8c914ea8d2dd9f8423eebc12a3763fa88f378c55
Suman Mondal <mondalsumanrock20@gmail.com>
2025-03-04 17:18:19 +0530

    refactor: update VerifyKycPageDetails component for improved authentication handling and API integration

--- lib/api.ts
+++ lib/api.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://127.0.0.1:8080";
 
-const api = axios.create({
-  baseURL: `${API_URL}/api`,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+const createApi = (authorization: string) => {
+  return axios.create({
+    baseURL: `${API_URL}/api`,
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": authorization
+    },
+  });
+};
 
-export default api;
+export default createApi;
