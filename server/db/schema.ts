afffb2f1ccd585c10184462743f1c473ca189a0b
Suman Mondal <mondalsumanrock20@gmail.com>
2025-03-06 01:57:53 +0530

    refactor: enhance user schema with role and KYC status enums, and add mobile and KYC documents fields

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,10 +1,37 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
 
-// Users Table
+export const userRole = pgEnum("userRole", ["SUPERADMIN", "ADMIN", "USER"]);
+export const kycStatus = pgEnum("kycStatus", [
+  "PENDING",
+  "APPROVED",
+  "REJECTED",
+  "APPLIED",
+]);
+
+// KYC Documents Type
+type KycDocuments = {
+  aadharCardFront: string;
+  aadharCardBack: string;
+  panCardFront: string;
+  panCardBack: string;
+  voterCardFont?: string;
+  voterCardBack?: string;
+  passportFront?: string;
+  passportBack?: string;
+  drivingLicenceFront?: string;
+  drivingLicenceBack?: string;
+};
+
 export const users = pgTable("users", {
-  id: text("id").primaryKey().unique().notNull(),
+  id: text("id").primaryKey().notNull(),
   clerkId: text("clerkId").unique().notNull(),
   name: text("name").notNull(),
   email: text("email").unique().notNull(),
+  mobile: text("mobile").unique(),
   avatar: text("avatar"),
+  role: userRole("role").default("USER").notNull(),
+  kycStatus: kycStatus("kycStatus").default("PENDING").notNull(),
+  kycDocuments: text("kycDocuments").$type<KycDocuments>(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
 });
