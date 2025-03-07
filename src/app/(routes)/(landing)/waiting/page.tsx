353f61c04e78bbf1ee163568ad7f5ed0b299bd15
gourab8389 <deyg6988@gmail.com>
2025-03-08 03:01:37 +0530

    feat: enhance Waiting page styling and refactor Navbar layout for improved responsiveness

--- src/app/(routes)/(landing)/waiting/page.tsx
+++ src/app/(routes)/(landing)/waiting/page.tsx
@@ -16,15 +16,15 @@ const WaitingPage = () => {
         />
       </div>
       <div className="flex flex-col gap-3 items-center text-center justify-center">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-2xl font-bold text-brandblack">
           Hang Tight! Our admin is reviewing your request
         </h1>
-        <p className="md:w-[70%]">
+        <p className="md:w-[70%] text-brandblack">
           Thank you for your application! Our team is reviewing your request for
           the admin role. Once approved, you will receive access to your
           dashboard. We appreciate your patience!
         </p>
-        <Link href={"/"} className="flex items-center gap-2 bg-brandred rounded-md px-3 py-2 text-white w-fit">
+        <Link href={"/"} className="flex items-center font-medium gap-2 bg-brandred rounded-md px-3 py-2 text-white w-fit">
         Contact Us
         <ChevronRight className="w-5 h-5" color="white"/>
         </Link>
